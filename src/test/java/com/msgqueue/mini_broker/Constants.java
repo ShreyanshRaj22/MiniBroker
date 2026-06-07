e766087548c3b6f2cb9aad2633c2eed45d2ecfaf
ShreyanshRaj22 <211230051@nitdelhi.ac.in>
2026-06-08 00:46:19 +0530

    Integration test added for produce(), consume(), commit() flow

--- src/test/java/com/msgqueue/mini_broker/Constants.java
+++ src/test/java/com/msgqueue/mini_broker/Constants.java
@@ -14,6 +14,8 @@ public class Constants {
 	public static final String CONSUMER_2_ID = "consumer-2";
 	
 	public final static String KEY_1 = "24";
+	public final static String KEY_2 = "25";
+
 	public final static String PAYLOAD_STRING = "payload-1";
 	public final static String PAYLOAD_STRING_2 = "payload-2";
 	public final static int LIMIT = 3;
--- src/test/java/com/msgqueue/mini_broker/flows/ConsumerGroupFlowTest.java
+++ src/test/java/com/msgqueue/mini_broker/flows/ConsumerGroupFlowTest.java
@@ -1,8 +1,15 @@
 package com.msgqueue.mini_broker.flows;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import com.msgqueue.mini_broker.Constants;
 import com.msgqueue.mini_broker.broker.Broker;
+import com.msgqueue.mini_broker.model.Message;
 import com.msgqueue.mini_broker.partitioner.HashPartitioner;
 import com.msgqueue.mini_broker.partitioner.Partitioner;
 import com.msgqueue.mini_broker.service.ConsumerService;
@@ -10,22 +17,100 @@ import com.msgqueue.mini_broker.service.GroupCoordinatorService;
 import com.msgqueue.mini_broker.service.ProducerService;
 
 public class ConsumerGroupFlowTest {
-	private Broker broker;
-	private GroupCoordinatorService coordinatorService;
-	private ConsumerService consumerService;
-	private ProducerService producerService;
-	private Partitioner partitioner;
 
-	@BeforeEach
-	void setup(){
-		broker = new Broker();
-		coordinatorService = new GroupCoordinatorService(broker);
-		partitioner = new HashPartitioner();
-		producerService = new ProducerService(broker, partitioner);
-		consumerService = new ConsumerService(broker, coordinatorService);
-	}
+    private Broker broker;
+    private GroupCoordinatorService coordinatorService;
+    private ConsumerService consumerService;
+    private ProducerService producerService;
+    private Partitioner partitioner;
 
-	@Test
-	void shouldProduceConsumeAndCommitOffsets(){
-	}
+    @BeforeEach
+    void setup() {
+        broker = new Broker();
+
+        coordinatorService =
+                new GroupCoordinatorService(broker);
+
+        partitioner = new HashPartitioner();
+
+        producerService =
+                new ProducerService(
+                        broker,
+                        partitioner
+                );
+
+        consumerService =
+                new ConsumerService(
+                        broker,
+                        coordinatorService
+                );
+    }
+
+    @Test
+    void shouldProducePollCommitAndPollAgain() {
+
+        broker.createTopic(
+                Constants.TOPIC_NAME,
+                Constants.THREE_PARTITIONS
+        );
+
+        // Register consumer
+        coordinatorService.registerConsumer(
+                Constants.GROUP_1_ID,
+                Constants.CONSUMER_1_ID,
+                Constants.TOPIC_NAME
+        );
+
+        // Produce messages
+        producerService.produce(
+                Constants.TOPIC_NAME,
+                Constants.KEY_1,
+                Constants.PAYLOAD_STRING
+        );
+
+        producerService.produce(
+                Constants.TOPIC_NAME,
+                Constants.KEY_2,
+                Constants.PAYLOAD_STRING_2
+        );
+
+        // First poll
+        List<Message> firstPoll =
+                consumerService.poll(
+                        Constants.GROUP_1_ID,
+                        Constants.CONSUMER_1_ID,
+                        Constants.TOPIC_NAME,
+                        10
+                );
+
+        assertEquals(2, firstPoll.size());
+
+        // Commit offsets for all consumed messages
+        for (Message message : firstPoll) {
+
+            int partitionId =
+                    partitioner.partition(
+                            message.getPartitionKey(),
+                            Constants.THREE_PARTITIONS
+                    );
+
+            consumerService.commit(
+                    Constants.GROUP_1_ID,
+                    Constants.TOPIC_NAME,
+                    partitionId,
+                    message.getOffset() + 1
+            );
+        }
+
+        // Poll again
+        List<Message> secondPoll =
+                consumerService.poll(
+                        Constants.GROUP_1_ID,
+                        Constants.CONSUMER_1_ID,
+                        Constants.TOPIC_NAME,
+                        10
+                );
+
+        assertEquals(0, secondPoll.size());
+    }
 }
